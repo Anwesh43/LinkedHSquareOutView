441b9ab582d8df6ad23e1d4b0f49dc54d4cd590d
Anwesh43 <anweshthecool0@gmail.com>
2021-06-27 03:03:22 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/liinkedhsquareoutview/MainActivity.kt
+++ app/src/main/java/com/example/liinkedhsquareoutview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.liinkedhsquareoutview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.hsquareoutview.HSquareOutView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HSquareOutView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
