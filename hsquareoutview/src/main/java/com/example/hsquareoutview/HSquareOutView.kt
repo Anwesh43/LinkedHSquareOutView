97a4bdfb93af5a469d7b1919d42410d48cebd049
Anwesh43 <anweshthecool0@gmail.com>
2021-06-27 01:30:07 +0530

    adding HSquareOutView class

--- hsquareoutview/src/main/java/com/example/hsquareoutview/HSquareOutView.kt
+++ hsquareoutview/src/main/java/com/example/hsquareoutview/HSquareOutView.kt
@@ -70,3 +70,19 @@ fun Canvas.drawHSONode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHSquareOut(scale, w, h, paint)
 }
+
+class HSquareOutView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
